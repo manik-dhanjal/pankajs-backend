92f2f2e2b77adcad9202eccd51e142b5c65f2f2c
Manik Dhanjal <mdhanja1@ford.com>
2023-05-21 00:27:53 +0530

    api fixed

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+    res.send("hello")
   })
 app.listen(process.env.PORT || 3000);
 module.exports = app
